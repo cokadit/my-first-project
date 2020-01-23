6e3fb669efa24a390296ac9cdb6e24fabe6da404
cokadit <cokadit1@gmail.com>
2020-01-23 13:16:00 +0800

    belajar sqlite

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
     public function list()
     {
-        $customers = [
-            'John Doe',
-            'Jane Doe',
-            'Bob the builder',
-            'Test'
-        ];
+        $customers = Customer::all();
 
         return view('internals.customers',compact('customers'));
 
