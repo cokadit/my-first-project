37420790088a4dcd5357caed910e92c01a6804d5
cokadit <cokadit1@gmail.com>
2020-02-26 15:20:21 +0800

    belajar Artisan Command(closure)

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use App\Company;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,15 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('contact:company-clean',function(){
+    $this->info('cleaning !');
+
+    Company::whereDoesntHave('customers')
+    ->get()
+    ->each(function ($company){
+        $company->delete();
+
+        $this->warn('Deleted' . $company->name);
+    });
+})->describe('Clean up unused companies');
