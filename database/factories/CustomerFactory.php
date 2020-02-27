897d95e0d1bc08f7a61305f21e7759df538d9adc
cokadit <cokadit1@gmail.com>
2020-02-27 15:33:18 +0800

    belajar database & table seeder

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Company;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Customer::class, function (Faker $faker) {
+    return [
+        'company_id'=> factory(Company::class)->create(),
+        'name'=>$faker->name,
+        'email'=>$faker->unique()->safeEmail,
+        'active'=>1,
+    ];
+});
